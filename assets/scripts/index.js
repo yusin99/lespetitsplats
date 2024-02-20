7f1c7c0357dc431919a2185ddb92d4ab5446f9f9
Yusin <sunkatabeckama@gmail.com>
2024-02-20 12:47:27 +0100

    Initializing the factories

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,24 @@
-import { cardFactory } from './factories/cardFactory.js'
-import { filterFactory } from './factories/filterFactory.js'
+import { RecipeFactory } from './factories/RecipeFactory.js'
+import { FilterFactory } from './factories/filterFactory.js'
+import { debounce, handleCross } from './utils/index.js'
 
-cardFactory().init()
-filterFactory().init()
+/**
+ * This script initializes recipe and filter factories, attaches event listeners to input elements,
+ * and debounces the update of recipe cards based on user input.
+ */
+
+// Initialize recipe and filter factories
+export const recipeFactory = RecipeFactory()
+export const filterFactory = FilterFactory()
+
+// Initialize recipe factory
+recipeFactory.init()
+
+// Initialize filter factory
+filterFactory.init()
+
+// Debounced function to update recipe cards based on user input
+const debouncedUpdateCards = debounce((value) => {
+  recipeFactory.updateCards(value)
+}, 500)
+
